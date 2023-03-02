9387788931edbfb869a82fccbc135c1033a80fc1
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2023-03-02 13:55:13 -0600

    [ADD] start

---
+++ GuardaPalabras/GuardaPalabras/TableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewController.swift
+//  GuardaPalabras
+//
+//  Created by Ricardo Granja Chávez on 02/03/23.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
---
+++ GuardaPalabras/GuardaPalabras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuardaPalabras
+//
+//  Created by Ricardo Granja Chávez on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
