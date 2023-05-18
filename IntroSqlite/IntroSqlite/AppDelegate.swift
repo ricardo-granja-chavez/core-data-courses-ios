29d671b92838da10992e6d6d7bece4b3edac4ab4
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2023-05-18 15:23:31 -0600

    [ADD] sqlite components

---
+++ IntroSqlite/IntroSqlite/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  IntroSqlite
+//
+//  Created by Ricardo Granja Chávez on 18/05/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ IntroSqlite/IntroSqlite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroSqlite
+//
+//  Created by Ricardo Granja Chávez on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
