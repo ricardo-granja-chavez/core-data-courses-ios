cfca2805d32b682356fea4e92f02147096620894
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2023-05-18 17:32:30 -0600

    [ADD] UI

--- IntroSqlite/IntroSqlite/ViewController.swift
+++ IntroSqlite/IntroSqlite/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var modelTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var modelSearchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func save(_ sender: Any) {
+        if modelTextField.hasText && priceTextField.hasText {
+            print("Si")
+            self.startSaved()
+        } else {
+            print("No")
+        }
+    }
+    
+    @IBAction func search(_ sender: Any) {
+        if modelSearchTextField.hasText {
+            print("Sii")
+            self.startSearched()
+        } else {
+            print("Noo")
+        }
+    }
+    
+    func startSaved() {
+        
+    }
+    
+    func startSearched() {
+        
+    }
 }
 
